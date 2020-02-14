f200c8c65e24db20e8067f49f35a9512ec570dd8
Zakir Sajib <zakirsajib@gmail.com>
2020-02-14 10:03:34 +0000

    header responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,8 @@ import { Link, StaticQuery, graphql } from 'gatsby'
 import LogoBlack from '../img/logoBlack.svg'
 import LogoYellow from '../img/LogoYellow.svg'
 
+import TelIcon from '../img/click-to-call-icon.svg'
+
 import userConfig from '../../config'
 
 class Navbar extends Component {
@@ -24,13 +26,58 @@ class Navbar extends Component {
 	        <div>  
 	        {this.state.childVisible ?  
             <nav className="has-bg-black has-text-centered x-nav slide-down">
+            	
+            	<div className="mobile-menu is-mobile is-hidden-desktop columns has-text-left">
+            		<div className="column">
+	                  <Link
+	                    className="navbar-item"
+	                    to="/"
+	                  >
+	                    Home
+	                  </Link>
+	                  <Link
+	                    className="navbar-item"
+	                    to="work"
+	                  >
+	                    Work
+	                  </Link>
+	                  
+	                  <Link
+	                    className="navbar-item"
+	                    to="about"
+	                  >
+	                    About
+	                  </Link>
+	                  
+	                  <Link
+	                    className="navbar-item"
+	                    to="contact"
+	                  >
+	                    Contact
+	                  </Link>
+	                  
+	                  <Link
+	                    className="navbar-item"
+	                    to="blog"
+	                  >
+	                    Blog
+	                  </Link>
+	                </div>
+				</div>
+            	
             	<div className="container">
 	                <div className="navbar-brand level-item has-text-centered">
+	                  
+	                  <a className="is-hidden-desktop" style={{alignItems: `center`, display: `flex`}}><img src={TelIcon} alt="telephone icon" /></a>
+	                  
 	                  <a onClick={() => this.onClick()} className="logo">
 	                  {this.state.childVisible ? 
 	                  <img src={LogoYellow} alt="X Trademark Yellow logo" />
 	                  : <img src={LogoBlack} alt="X Trademark Black logo" /> }
 	                  </a>
+	                  
+	                  <a className="is-hidden-desktop" style={{alignItems: `center`, display: `flex`}} href="mailto:info@trademarkevents.com"><img src={TelIcon} alt="email icon" /></a>
+	                  
 	                </div>
 	                <div id="navMenu" className="level-item">
 	                  {this.state.childVisible ?
@@ -73,7 +120,7 @@ class Navbar extends Component {
 	              </div>
               </div>
             </nav>
-            : <nav className="has-text-centered x-nav posAbsolute slide-up"> 
+            : <nav className="has-text-centered x-nav slide-up"> 
                 <div className="container">
 	                <div className="navbar-brand level-item has-text-centered">
 	                  <a onClick={() => this.onClick()} className="logo">
