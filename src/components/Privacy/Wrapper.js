937075ce3bfd86a754b78da1db165cc0ea83515a
Zakir Sajib <zakirsajib@gmail.com>
2020-02-14 04:09:42 +0000

    no message

--- src/components/Privacy/Wrapper.js
+++ src/components/Privacy/Wrapper.js
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 const PrivacyStyle = styled.div`
 	//padding-top: 25%;
 	
+	.column.col-left{
+		border-right: 1px solid #4E6F7B;
+	}
+	
 	h2{
 		font-size: 60.6px;
 		padding-bottom: 10px;
@@ -10,12 +14,15 @@ const PrivacyStyle = styled.div`
 	h3{
 		font-size: 28px;
 		line-height: 32px;
-		padding-bottom: 40px;
+		padding-bottom: 20px;
+		padding-top: 20px;
+		border-top: 1px solid #4E6F7B;
 	}
 	p{	
 		font-weight: 400;
 		font-size: 14px;
 		line-height: 24px;
+		padding-bottom: 20px;
 	}
 
 `;
--- src/components/Privacy/index.js
+++ src/components/Privacy/index.js
@@ -16,39 +16,54 @@ const Privacy = () => {
 		    					<p className="has-text-white">We value your business and we know your privacy is important to you. Accordingly, we have implemented this Privacy Policy to inform you of the information that we collect from you, how we collect it and what we do with it. By using this website you are accepting the practices described in this Privacy Policy.</p>
 		    				</div>
 		    			</div>
-		    			<div className="columns" style={{borderTop: `1px solid #fff`}}>
-		    				<div className="column" style={{borderRight: `1px solid #fff`, borderBottom: `1px solid #fff`, paddingLeft: `1.5rem`, paddingRight: `1.5rem`}}>
-		    					<h3 className="has-text-left has-text-white">How and when we collect information</h3>
+		    			<div className="columns" style={{borderTop: `1px solid #4E6F7B`, borderBottom: `1px solid #4E6F7B`}}>
+		    				<div className="column col-left" style={{paddingLeft: `1.5rem`, paddingRight: `1.5rem`}}>
+		    					<h3 className="has-text-left has-text-white" style={{border:`0`}}>How and when we collect information</h3>
 		    					<p className="has-text-white">For a better experience, we may collect certain personally identifiable information, including but not limited to your name, company, and email address. The information that we collect will be used to contact or identify you.</p>
+		    					<br />
+		    					
+		    					<h3 className="has-text-left has-text-white">What we do with your information</h3>
+		    					<p className="has-text-white">We are committed to protecting your privacy. Under no circumstances do we rent, trade or share your e-mail address with any other company for their marketing purposes without your consent. We use your personal information for internal purposes such as processing and keeping you informed of your order. You may, from time to time, receive information from us about new services we think you’ll find valuable. If you take advantage of special offers made by our marketing partners, we may, with your consent, transfer your personal information to them.</p>
+<p className="has-text-white">We may disclose the personal and non-personal information of our customers to any successor-in-interest of ours, such as a company that acquires our business. We may need to disclose your personal information when required by law or when we have a good-faith belief that such action is necessary to comply with a judicial proceeding, a court order or legal process.</p><br/ >
+								
+								<h3 className="has-text-left has-text-white">Your choice to opt-out of certain communications</h3>
+		    					<p className="has-text-white">If you are no longer interested in receiving e-mail announcements and other marketing information from us, please e-mail your request to info@trademarkevents.com. Please include your complete name and e-mail address. Note that you may still receive account related communications from us.</p>
+		    					
+		    					<br />
+		    					
+		    					<h3 className="has-text-left has-text-white">IP address and cookies</h3>
+		    					<p className="has-text-white">We also collect “non-personal” information from you, such as your browser type, the URL of the previous web site you visited, your ISP, operating system, and your IP address. “Non-personal” information cannot be easily used to personally identify you. We may use your non-personal information to help diagnose problems with our server and to administer our website. Your non-personal information may also be used to gather broad demographic information and to recognize customer traffic patterns and website usage. This information aids us in merchandising and in developing the design and layout of the website.</p>
+<p className="has-text-white">Our website also uses “cookies” to collection information and to improve your experience. Cookies are files with small amount of data that is commonly used an anonymous unique identifier. These are sent to your browser from the website that you visit and are stored on your computer’s hard drive. You have the option to either accept or refuse these cookies, and know when a cookie is being sent to your computer. If you choose to refuse our cookies, you may not be able to use some portions of our service.</p>
+		    					
+		    					
+		    					
 		    				</div>
-		    				<div className="column" style={{borderBottom: `1px solid #fff`, paddingLeft: `1.5rem`, paddingRight: `1.5rem`}}>
-		    					<h3 className="has-text-left has-text-white">Links to other websites</h3>
+		    				<div className="column col-right" style={{paddingLeft: `1.5rem`, paddingRight: `1.5rem`}}>
+		    					<h3 className="has-text-left has-text-white" style={{border:`0`}}>Links to other websites</h3>
 		    					<p className="has-text-white">Our Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. We have no control over, and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.</p>
+		    				<br />
+		    				<h3 className="has-text-left has-text-white">Protecting your information</h3>
+		    					<p className="has-text-white">The security of your personal information is very important to us. We follow generally accepted industry standards to protect the personal information submitted to us from unauthorized access or disclosure, both during transmission and once we receive it. However, no method of transmission over the Internet, or method of electronic storage, is 100% secure. Therefore, while we strive to use commercially acceptable means to protect your personal information, we cannot guarantee its absolute security. We assume no liability for any disclosure of data due to errors in transmission, unauthorized third party access or other acts of third parties, or other acts or omissions.</p>
+		    					
+		    				<br />
+		    				
+		    				<h3 className="has-text-left has-text-white">Protecting your information</h3>
+		    					<p className="has-text-white">The security of your personal information is very important to us. We follow generally accepted industry standards to protect the personal information submitted to us from unauthorized access or disclosure, both during transmission and once we receive it. However, no method of transmission over the Internet, or method of electronic storage, is 100% secure. Therefore, while we strive to use commercially acceptable means to protect your personal information, we cannot guarantee its absolute security. We assume no liability for any disclosure of data due to errors in transmission, unauthorized third party access or other acts of third parties, or other acts or omissions.</p>
+		    					
+		    				<h3 className="has-text-left has-text-white">Children’s privacy</h3>
+		    					<p className="has-text-white">Our services are not intended for anyone under the age of 13. We do not knowingly collect personal identifiable information from children under 13. In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to do necessary actions.</p>
+		    					<br />
+		    				<h3 className="has-text-left has-text-white">Changes to our Privacy Policy</h3>
+		    					<p className="has-text-white">If we change our Privacy Policy, we will post a new version on our Website, which will become effective when posted. The new policy will apply to all current and past users of the Website and will replace any prior policies that are inconsistent with it.</p>	
+		    				
 		    				</div>
 		    			</div>
+		    			
+		    			
+		    			
 		    			<div className="columns">
-		    				<div className="column" style={{borderRight: `1px solid #fff`, borderBottom: `1px solid #fff`, paddingLeft: `1.5rem`, paddingRight: `1.5rem`}}>
-		    					<h3 className="has-text-left has-text-white">How and when we collect information</h3>
-		    					<p className="has-text-white">For a better experience, we may collect certain personally identifiable information, including but not limited to your name, company, and email address. The information that we collect will be used to contact or identify you.</p>
-		    				</div>
-		    				<div className="column" style={{borderBottom: `1px solid #fff`, paddingLeft: `1.5rem`, paddingRight: `1.5rem`}}>
-		    					<h3 className="has-text-left has-text-white">Links to other websites</h3>
-		    					<p className="has-text-white">Our Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. We have no control over, and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.</p>
-		    				</div>
-		    			</div>
-		    			<div className="columns">
-		    				<div className="column" style={{borderRight: `1px solid #fff`, paddingLeft: `1.5rem`, paddingRight: `1.5rem`}}>
-		    					<h3 className="has-text-left has-text-white">How and when we collect information</h3>
-		    					<p className="has-text-white">For a better experience, we may collect certain personally identifiable information, including but not limited to your name, company, and email address. The information that we collect will be used to contact or identify you.</p>
-		    				</div>
-		    				<div className="column" style={{paddingLeft: `1.5rem`, paddingRight: `1.5rem`}}>
-		    					<h3 className="has-text-left has-text-white">Links to other websites</h3>
-		    					<p className="has-text-white">Our Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. We have no control over, and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.</p>
-		    				</div>
-		    			</div>
-		    			<div className="columns" style={{borderTop: `1px solid #fff`, paddingTop: `1.5rem`}}>
 		    				<div className="column">
-		    					<h3 className="has-text-left has-text-white">Contact us</h3>
+		    					<h3 className="has-text-left has-text-white" style={{border: `0`}}>Contact us</h3>
 		    					<p className="has-text-white">If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us at <a href="mailto:info@trademarkevents.com">info@trademarkevents.com</a>.</p>
 		    				</div>
 		    			</div>
